354466da7d35e4c37a931a70dee2a520398c2094
Joel <9orangetigers@gmail.com>
2022-05-28 23:52:11 -0700

    Complete exercises 8-12

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    const letters = a.toLowerCase().split(/[^a-z]/).join('').split('');
+    console.log(letters.join(''));
+    console.log(letters.reverse().join(''));
+    return letters.join('') === letters.reverse().join('');
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((acc, p) => {
+        const currentDate = new Date();
+        const currentYear = currentDate.getFullYear();
+        return age(p, currentYear) > age(acc, currentYear) ? p : acc;
+    }, {
+        yearOfBirth: 0,
+        yearOfDeath: 0,
+    });
 };
+
+const age = function(p, currentYear) {
+    return Number.isInteger(p.yearOfDeath) ? p.yearOfDeath - p.yearOfBirth : currentYear - p.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
